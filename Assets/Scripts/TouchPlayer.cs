5304231b277133617b548160d962be2d92369095
Jean-Baptiste Demenet <demenet.jb@hotmail.fr>
2020-04-19 11:27:39 +0100

    anilm implemented

--- Assets/Scripts/TouchPlayer.cs
+++ Assets/Scripts/TouchPlayer.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class TouchPlayer : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _touchParticleSystem;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            Instantiate(_touchParticleSystem, gameObject.transform);
+            _anim.SetTrigger("Jump");
         }
     }
 }
